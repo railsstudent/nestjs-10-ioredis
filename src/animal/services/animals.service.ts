f36d45de04edb3704f7d862f2551ecc5b4d342e2
Connie Leung <connie.leung@diginex.com>
2023-11-28 17:48:44 +0800

    refactor: add helper function to set key/value to redis

--- src/animal/services/animals.service.ts
+++ src/animal/services/animals.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRedis } from '@songkeys/nestjs-redis';
 import { Redis } from 'ioredis';
 import { env } from '~configs/env.config';
-import { getCacheValue, stringifyCacheValue } from '~core/helpers/cache-value.helper';
+import { getCacheValue, setCacheValue } from '~core/helpers/cache-value.helper';
 import { CreateAnimalDto } from '../dtos/create-animal.dto';
 import { DeleteAnimalDto } from '../dtos/delete-animal.dto';
 
@@ -11,11 +11,7 @@ export class AnimalsService {
   constructor(@InjectRedis() private cache: Redis) {}
 
   async cacheValue({ name, color }: CreateAnimalDto): Promise<void> {
-    if (env.REDIS.TTL) {
-      await this.cache.set(name, stringifyCacheValue(color), 'EX', env.REDIS.TTL);
-    } else {
-      await this.cache.set(name, stringifyCacheValue(color));
-    }
+    await setCacheValue(this.cache, name, color, env.REDIS.TTL);
   }
 
   async getValue(key: string): Promise<string> {
--- src/core/helpers/cache-value.helper.ts
+++ src/core/helpers/cache-value.helper.ts
@@ -1,8 +1,15 @@
 import Redis from 'ioredis';
 
-export async function getCacheValue<T>(redisCache: Redis, key: string) {
+export async function getCacheValue<T>(redisCache: Redis, key: string): Promise<T> {
   const val = await redisCache.get(key);
   return val === undefined || val === null ? undefined : (JSON.parse(val) as T);
 }
 
-export const stringifyCacheValue = (value: unknown) => JSON.stringify(value) || '"undefined"';
+export async function setCacheValue(redisCache: Redis, key: string, value: unknown, ttl = 0): Promise<void> {
+  const stringifiedValue = JSON.stringify(value) || '"undefined"';
+  if (ttl !== undefined && ttl !== 0) {
+    await redisCache.set(key, stringifiedValue, 'EX', ttl);
+  } else {
+    await redisCache.set(key, stringifiedValue);
+  }
+}
